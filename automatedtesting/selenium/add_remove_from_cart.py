a19f2b26381d6556b72372458ad81eb53570be6e
duypq154 <duypq154@gmail.com>
2023-07-11 15:00:12 +0700

    update selenium4

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,114 +2,62 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
-import logging
 import datetime
+import logging
 
- 
-# Start browser and login using standard_user
-def functional_ui_test(user, password):
 
+def timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return (ts + '\t')
+
+# Start the browser and login with standard_user
+def login(user, password):
     logging.basicConfig(filename="./selenium.log", format="%(asctime)s %(message)s", filemode="w", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
     logging.info('Starting the browser...')
-    
+
     options = ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument("--headless") 
     options.add_argument("--remote-debugging-port=9222")
 
-    #options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
-
-    # Login to website
-    logging.info('Browser started. Access to the demo page.')
-
+    logging.info(timestamp() + 'Browser has started. Logging in...')
     driver.get('https://www.saucedemo.com/')
+    # login
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
+    product_label = driver.find_element(By.CSS_SELECTOR, "span.title").text
+    assert "Products" in product_label
+    logging.info(timestamp() + 'Login with username {:s} and password {:s} successfully.'.format(user, password))
+    return driver
 
-    driver.find_element(By.CSS_SELECTOR,"input[id='user-name']").send_keys(user)
-
-    driver.find_element(By.CSS_SELECTOR,"input[id='password']").send_keys(password)
-
-    driver.find_element(By.CSS_SELECTOR,"input[id='login-button']").click()
-
-    path_content_div = "div[id='page_wrapper'] > div[id='contents_wrapper']"
-
-    results = driver.find_element(By.CSS_SELECTOR,path_content_div + " > div[class='header_container'] > div[class='header_secondary_container'] > span").text
-
-    assert "Products" in results
-    logging.info("Successfully logged in as user: " + user)
-
-
-    # Add Items to Shopping Cart
-    logging.info("Starting the shopping...")
-
-    path_inventory_item = "//*[@id='inventory_container']/*[@class='inventory_list']/div[@class='inventory_item']"
-
-    product_items = driver.find_elements(By.XPATH,path_inventory_item)
-
-    assert len(product_items) == 6
-
-    logging.info("Successfully found 6 product items.")    
-
-    for item in product_items:
-
-        element = "a[id='item_" + str(item) + "_title_link']"  # Get the product URL
+def add_cart(driver, n_items):
+    for i in range(n_items):
+        element = "a[id='item_" + str(i) + "_title_link']"  # Get the product URL
         driver.find_element(By.CSS_SELECTOR, element).click()  # Click the found URL
         driver.find_element(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click()  # Add product to the cart
         product = driver.find_element(By.CSS_SELECTOR, "div.inventory_details_name").text  # Get the product name
+        logging.info(timestamp() + product + " added to shopping cart.")  # Show what product has been added
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()  # Go Back
+    logging.info(timestamp() + '{:d} items were added to shopping cart.'.format(n_items))
 
-        #product_item_name = item.find_element(By.XPATH,"//div[@class='inventory_item_name']")
-
-        item.find_element(By.XPATH,"//button[contains(text(),'Add to cart')]").click()
-
-        #print("Succesfully added to shopping cart: " + product_item_name.text)
-
-        #logging.info("Succesfully added to shopping cart: " + product_item_name.text)
-        logging.info("Succesfully added to shopping cart: " + product)
-
-    path_shopping_cart_link = "//*[@id='shopping_cart_container']"
-
-    path_shopping_cart_badge = path_shopping_cart_link + "//span[@class='shopping_cart_badge']"
-
-    shopping_cart_total_items = driver.find_element(By.XPATH,path_shopping_cart_badge).text
-
-    assert '6' == shopping_cart_total_items
-
-    logging.info("Succesfully added to shopping cart: 6 items in total")
-
- 
-    # Remove Items from Shopping Cart
-    logging.info("A spouse came in, need to destroy the the evidence... ;-)")
-
-    driver.find_element(By.XPATH,path_shopping_cart_link).click()
-
-    path_cart_title = "//*[@id='header_container']/div[@class='header_secondary_container']/span"
-
-    cart_title = driver.find_element(By.XPATH,path_cart_title).text
-
-    assert 'Your Cart' in cart_title
-
-    logging.info("Successfully entered the shopping cart page.")
-
-    path_cart_item_remove_buttons = "//*[@id='cart_contents_container']//div[@class='cart_list']//div[@class='cart_item']"
-
-    remove_item_buttons = driver.find_elements(By.XPATH,path_cart_item_remove_buttons)
+def remove_cart(driver, n_items):
+    for i in range(n_items):
+        element = "a[id='item_" + str(i) + "_title_link']"
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory").click()
+        product = driver.find_element(By.CSS_SELECTOR, "div.inventory_details_name").text
+        logging.info(timestamp() + product + " removed from shopping cart.")  # Display message saying which product was added
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+    logging.info(timestamp() + '{:d} items are all removed from shopping cart.'.format(n_items))
 
 
-    for remove_button in remove_item_buttons:
-
-        shopping_cart_item_name = remove_button.find_element(By.XPATH,"//*[@class='inventory_item_name']").text
-
-        remove_button.find_element(By.XPATH,"//button[text()='Remove']").click()
-
-        logging.info("Succesfully removed an item from shopping cart: " + shopping_cart_item_name)
-
-    shopping_cart_total_items = driver.find_elements(By.XPATH,path_shopping_cart_badge)
-
-    assert 0 == len(shopping_cart_total_items)
-
-    logging.info("Succesfully removed all items from shopping cart.")
-
-
-    logging.info('UI tests are all completed')
-
-functional_ui_test('standard_user', 'secret_sauce')
+if __name__ == "__main__":
+    N_ITEMS = 6
+    TEST_USERNAME = 'standard_user'
+    TEST_PASSWORD = 'secret_sauce'
+    driver = login(TEST_USERNAME, TEST_PASSWORD)
+    add_cart(driver, N_ITEMS)
+    remove_cart(driver, N_ITEMS)
+    logging.info(timestamp() + 'UI tests are all completed')
